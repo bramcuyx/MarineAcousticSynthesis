9d61057596a247ba8807e4b4bf5beba541c35a86
Bram Cuyx <bram.cuyx@vliz.be>
2026-03-12 11:08:06 +0000

    fix: correct path to config file

--- demo/debug.py
+++ demo/debug.py
@@ -6,7 +6,7 @@ import yaml
 
 from uw_sim.audio_simulator import AudioSimulator, DataSet
 
-config = yaml.safe_load(pathlib.Path("config.yaml").read_text())
+config = yaml.safe_load(pathlib.Path("../config.yaml").read_text())
 PATHS = config["paths"]
 backgrounds = pathlib.Path(PATHS["background"])
 events = pathlib.Path(PATHS["events"])
@@ -22,7 +22,7 @@ simulator = AudioSimulator(
     duration=10,
 )
 
-output_audio, metadata_file = simulator.simulate_audio(snr=10, num_events=3)
+output_audio, metadata_file = simulator.simulate_audio(snr=10, num_events=1)
 print(f"Output audio file: {output_audio}")
 
 print(f"Metadata file: {metadata_file}")
